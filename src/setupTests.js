f3d4021dd064db2713d64b593c283b5960981a26
Tieson18 <tieson360@gmail.com>
2026-02-23 18:06:23 -0600

    Completed Assessment project

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
-const { TextEncoder, TextDecoder } = require('util');
+const { TextEncoder, TextDecoder } = require("util");
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-require('@testing-library/jest-dom');
+require("@testing-library/jest-dom");
